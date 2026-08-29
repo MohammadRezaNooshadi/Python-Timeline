59bc0d181db9930159061aa23fadfb577c85289e
Mohammad Reza Nooshadi <mohammad.reza.nooshadi@gmail.com>
2026-08-29 10:46:00 +0000

    define challenge

---
+++ Challenge_1.py
@@ -0,0 +1,7 @@
+"""
+Challenge: Iterative Number Analyzer
+Write a Python script that takes a single numerical input from the user and performs basic evaluation:
+1. Prompt the user to enter a number.
+2. Check whether the number is positive, negative, or zero.
+3. If the number is non-zero, determine whether it is even or odd.
+"""
