83df1809f58e6919113ff058a0b63cbbd703d379
Mohammad Reza Nooshadi <mohammad.reza.nooshadi@gmail.com>
2026-08-29 10:53:16 +0000

    Solved ✅

--- Challenge_1.py
+++ Challenge_1.py
@@ -5,3 +5,22 @@ Write a Python script that takes a single numerical input from the user and perf
 2. Check whether the number is positive, negative, or zero.
 3. If the number is non-zero, determine whether it is even or odd.
 """
+while True:
+    try:
+        num = int(input("Please enter a number: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+if num != 0:
+    if num % 2 == 0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
