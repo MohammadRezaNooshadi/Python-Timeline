bbcc77cf8e12dd161bc8fffe5f907115248753df
Mohammad Reza Nooshadi <mohammad.reza.nooshadi@gmail.com>
2026-08-29 11:03:10 +0000

    define challenge

---
+++ Challenge_2.py
@@ -0,0 +1,19 @@
+"""
+Challenge 2: Multi-Number Collector & Summary Report
+Write a Python script that continuously collects numbers from the user and generates a statistical summary at the end.
+
+Requirements:
+1. Interactive Loop: Keep asking the user to input a number until they explicitly type 'done' or 'exit'.
+
+2. Input Validation:
+
+    If the user enters a valid integer, save it.
+    If the user types 'done' or 'exit', break the loop.
+    If the input is invalid (e.g., text), print an error message and ask again without crashing.
+
+3. Data Storage & Analysis: Store all valid numbers in a list. Once the loop ends, display:
+
+    The complete list of entered numbers.
+    Total count of valid numbers.
+    Sum, minimum, and maximum values of the list.
+"""
